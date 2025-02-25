0e3519fabc1a04bc7379d82898d6cc1b10930036
braden <leachbr787@northwested.org>
2025-02-25 13:28:38 -0500

    Update game.py

--- game.py
+++ game.py
@@ -87,7 +87,7 @@ def main():
 
 
         #-- other shapes --#
-        while pygame.mouse.get_pressed()[0] == True:
+        if pygame.mouse.get_pressed()[0] == True:
             # --- Circle Drawn --- #
             circle_center = (380, 295)
             circle_radius = 200
